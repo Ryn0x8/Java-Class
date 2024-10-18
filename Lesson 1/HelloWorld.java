5d22e416eebe77946a4ad62bc5bce34f8399fff4
InfernoblzDEV <saralthapaliya1@gmail.com>
2024-10-18 18:45:00 +0545

    Lesson 1 completed

---
+++ Lesson 1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("This is my first Java program.");
+        System.out.println("I'm glad to be here!");
+        System.out.println("Thank you for joining me.");
+    }
+}
