155d2cc52eb881d653cbe3c16a8fd2ae8962c39a
InfernoblzDEV <saralthapaliya1@gmail.com>
2024-10-22 20:26:51 +0545

    Completed

---
+++ Lesson 2/Methods.java
@@ -0,0 +1,14 @@
+public class Methods {
+    public static void main (String[] args){
+        System.out.println("This is my maggie recipe");
+        maggieRecipe();
+    }
+    private static void maggieRecipe(){
+        System.out.println("Boil 2 cups of water");
+        System.out.println("Add maggie masala");
+        System.out.println("Add veggies");
+        System.out.println("Cook for 2 minutes");
+        System.out.println("Maggie is ready");
+    }
+}
+
